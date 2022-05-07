923563ec03564642aa4b8a45d747d17c859e0bcf
purna45 <105117376+purna45@users.noreply.github.com>
2022-05-07 20:15:57 +0100

    first commit

---
+++ src/main/java/com/microservices/explore/Explore/MicroServices/ExploreMicroServicesApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.explore.Explore.MicroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreMicroServicesApplication.class, args);
+
+		System.out.println("Testing....!");
+	}
+
+}
---
+++ src/test/java/com/microservices/explore/Explore/MicroServices/ExploreMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.explore.Explore.MicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
