b7412f5d9dd947a7ca007e4ab44955cc7e5fb1b2
purna45 <mpurnachandra.java@gmail.com>
2022-05-08 01:03:53 +0100

    second change

---
+++ src/main/java/com/microservices/explore/Explore/MicroServices/testController.java
@@ -0,0 +1,10 @@
+package com.microservices.explore.Explore.MicroServices;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/employees")
+public class testController {
+
+
+}
